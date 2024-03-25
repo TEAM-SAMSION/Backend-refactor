6ceed209f2aaa366bbe9235659bccd09da9f3bd5
Sim-km <simkm0824@naver.com>
2024-03-25 17:48:59 +0900

    feat : mapper 애노테이션에 @Component 추가

--- Common-Module/src/main/java/com/pawith/commonmodule/annotation/Mapper.java
+++ Common-Module/src/main/java/com/pawith/commonmodule/annotation/Mapper.java
@@ -1,5 +1,7 @@
 package com.pawith.commonmodule.annotation;
 
+import org.springframework.stereotype.Component;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,5 +9,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Component
 public @interface Mapper {
 }
