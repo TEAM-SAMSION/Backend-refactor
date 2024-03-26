4c3f167b892bc23573a86dd76509d564341ead77
Sim-km <simkm0824@naver.com>
2024-03-27 02:09:13 +0900

    refactor : 사용자 정보 수정 메소드 let을 통해 변경할 수 있도록 수정

--- Domain-Module/User-Module/User-Domain/src/main/java/com/pawith/userdomain/service/user/UserModifier.kt
+++ Domain-Module/User-Module/User-Domain/src/main/java/com/pawith/userdomain/service/user/UserModifier.kt
@@ -3,13 +3,23 @@ package com.pawith.userdomain.service.user
 import com.pawith.commonmodule.annotation.DomainService
 import com.pawith.userdomain.entity.User
 import com.pawith.userdomain.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
 
 @DomainService
 class UserModifier(
     private val userRepository: UserRepository
 ) {
-    fun modifyNickname(user: User, newNickname: String){
-        user.updateNickname(newNickname);
-        userRepository.save(user);
+    fun modifyNickname(userId: Long, newNickname: String){
+        userRepository.findByIdOrNull(userId)?.let {
+            it.updateNickname(newNickname)
+            userRepository.save(it)
+        }
+    }
+
+    fun modifyProfileImage(userId: Long, newProfileImage: String){
+        userRepository.findByIdOrNull(userId)?.let {
+            it.updateProfileImage(newProfileImage)
+            userRepository.save(it)
+        }
     }
 }
