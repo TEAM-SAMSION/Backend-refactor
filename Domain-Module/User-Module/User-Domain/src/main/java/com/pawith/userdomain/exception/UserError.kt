f07a35ef5f3141b88a04c0ad03a8286955f35f7c
Sim-km <simkm0824@naver.com>
2024-03-29 23:41:01 +0900

    refactor : UserError 클래스 Error 구현하도록 변경

--- Domain-Module/User-Module/User-Domain/src/main/java/com/pawith/userdomain/exception/UserError.kt
+++ Domain-Module/User-Module/User-Domain/src/main/java/com/pawith/userdomain/exception/UserError.kt
@@ -1,5 +1,6 @@
 package com.pawith.userdomain.exception
 
+import com.pawith.commonmodule.exception.Error
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 
@@ -7,8 +8,20 @@ enum class UserError(
     val message: String,
     val errorCode: Int,
     val httpStatus: HttpStatusCode
-) {
+) : Error {
     USER_NOT_FOUND("사용자를 찾을 수 없습니다.", 2000, HttpStatus.NOT_FOUND),
-    USER_AUTHORITY_NOT_FOUND("사용자 권한을 찾을 수 없습니다.", 2001, HttpStatus.NOT_FOUND),
     ACCOUNT_ALREADY_EXIST("이미 가입한 계정이 있습니다", 2002, HttpStatus.BAD_REQUEST);
+
+    override fun getErrorCode(): Int {
+        return errorCode
+    }
+
+    override fun getHttpStatusCode(): HttpStatusCode {
+        return httpStatus
+    }
+
+    override fun getMessage(): String {
+        return message
+    }
+
 }
