d4a5d32d01dfb0b417b37fb93a08138c753c892f
Sim-km <simkm0824@naver.com>
2024-03-29 23:10:43 +0900

    refactor : 엔티티의 생성, 삭제를 담당하는 BaseEntity 필드명 수정

--- Common-Module/src/main/java/com/pawith/commonmodule/domain/BaseEntity.java
+++ Common-Module/src/main/java/com/pawith/commonmodule/domain/BaseEntity.java
@@ -1,8 +1,11 @@
 package com.pawith.commonmodule.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,12 +15,15 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class BaseEntity {
 
     @CreatedDate
-    protected LocalDateTime createdAt;
+    @Column(updatable = false)
+    protected LocalDateTime createdDate;
 
     @LastModifiedDate
-    protected LocalDateTime updatedAt;
+    protected LocalDateTime updatedDate;
 
 }
